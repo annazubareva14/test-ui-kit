026562b7c9036837c190280f9fc889d3412e1053
annazubareva14 <annazubareva14@yandex.ru>
2024-05-07 14:24:47 +0300

    init app

---
+++ src/components/TestButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button class="test-button">test button</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TestButton'
+}
+</script>
+
+<style>
+.test-button {
+  position: relative;
+  width: 100%;
+  max-width: 400px;
+  min-height: 50px;
+  padding: 20px 30px;
+  border-radius: 10px;
+  border: 0 none;
+  outline: 0;
+  background-color: #1e87be;
+  color: #fff;
+  font-weight: 700;
+  font-size: 24px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+</style>
